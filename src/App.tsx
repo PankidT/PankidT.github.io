63877fe600b78fee692a4141c81ff7f01fe5fc14
Poramat <poramat.chianvichai@gmail.com>
2023-11-27 12:27:31 +0700

    remove children

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "./components/mode-toggle";
 import "./App.css";
 
-function App({ children }: { children: React.ReactNode }) {
+function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {children}
       <ModeToggle />
     </ThemeProvider>
   );
